2c4d956a001ed3a124495d0cad3c1a53f8ae2bb6
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-01 19:32:25 +0000

    Add main game loop and scores

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0,
+  computerScore = 0;
+
 function getComputerChoice() {
   let rng = Math.floor(Math.random() * 3) + 1;
 
@@ -15,21 +18,51 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === "rock") {
     if (computerSelection === "scissors") {
+      playerScore++;
       return "You won! " + playerSelection + " beats " + computerSelection;
     } else {
+      computerScore++;
       return "You lost! " + computerSelection + " beats " + playerSelection;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
+      playerScore++;
       return "You won! " + playerSelection + " beats " + computerSelection;
     } else {
+      computerScore++;
       return "You lost! " + computerSelection + " beats " + playerSelection;
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "paper") {
+      playerScore++;
       return "You won! " + playerSelection + " beats " + computerSelection;
     } else {
+      computerScore++;
       return "You lost! " + computerSelection + " beats " + playerSelection;
     }
+  } else {
+    computerScore++;
+    return "You lost - input a valid option!";
   }
 }
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(
+      playRound(prompt("Rock, paper or scissors?"), getComputerChoice())
+    );
+  }
+
+  console.log(
+    playerScore > computerScore
+      ? "You win!"
+      : playerScore < computerScore
+      ? "You lose!"
+      : "It's a draw!"
+  );
+
+  console.log("You had " + playerScore + " points.");
+  console.log("The computer had " + computerScore + " points.");
+}
+
+game();
