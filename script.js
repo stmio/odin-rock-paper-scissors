9868e5b19fc4e9ef2e24498f8cbb40dccef40cee
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-01 17:40:58 +0000

    Add logic for a single round

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function getComputerChoice() {
   if (rng === 2) return "paper";
   if (rng === 3) return "scissors";
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a draw! The computer chose " + computerSelection;
+  }
+
+  if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      return "You won! " + playerSelection + " beats " + computerSelection;
+    } else {
+      return "You lost! " + computerSelection + " beats " + playerSelection;
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You won! " + playerSelection + " beats " + computerSelection;
+    } else {
+      return "You lost! " + computerSelection + " beats " + playerSelection;
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      return "You won! " + playerSelection + " beats " + computerSelection;
+    } else {
+      return "You lost! " + computerSelection + " beats " + playerSelection;
+    }
+  }
+}
