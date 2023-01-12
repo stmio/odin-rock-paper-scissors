3fa752794b9188f3762b8714006578e2400b2e22
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-12 18:33:12 +0000

    End round after 5 points is reached

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundEnded = false;
 
 function getComputerChoice() {
   let rng = Math.floor(Math.random() * 3) + 1;
@@ -7,6 +8,10 @@ function getComputerChoice() {
   if (rng === 1) return "rock";
   if (rng === 2) return "paper";
   if (rng === 3) return "scissors";
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function getRoundOutcome(playerSelection, computerSelection) {
@@ -27,6 +32,8 @@ function getRoundOutcome(playerSelection, computerSelection) {
 function updateMessage(outcome, playerSelection, computerSelection) {
   const roundOutcome = document.getElementById("round-outcome");
   const roundInfo = document.getElementById("round-info");
+
+  playerSelection = capitalizeFirstLetter(playerSelection);
 
   if (outcome === "draw") {
     roundOutcome.textContent = "It's a draw.";
@@ -64,16 +71,58 @@ function updateScore(player, computer) {
   computerScore.textContent = computer;
 }
 
+function resetGame() {
+  playerScore = computerScore = 0;
+  roundEnded = false;
+
+  const endGameMessage = document.querySelector(".end-game");
+  const roundOutcome = document.getElementById("round-outcome");
+  const roundInfo = document.getElementById("round-info");
+  const playerEmoji = document.getElementById("player-selection");
+  const computerEmoji = document.getElementById("computer-selection");
+  const playerScoreCounter = document.getElementById("player-score");
+  const computerScoreCounter = document.getElementById("computer-score");
+
+  endGameMessage.style.display = "none";
+  roundOutcome.textContent = "Select an option!";
+  roundInfo.textContent = "First player to 5 points wins!";
+  playerEmoji.textContent = computerEmoji.textContent = "❔";
+  playerScoreCounter.textContent = computerScoreCounter.textContent = "0";
+}
+
 function playRound(player) {
   const computer = getComputerChoice();
   const outcome = getRoundOutcome(player, computer);
 
-  if (outcome === "won") playerScore++;
-  if (outcome === "lost") computerScore++;
+  if (!roundEnded) {
+    if (outcome === "won") playerScore++;
+    if (outcome === "lost") computerScore++;
 
-  updateMessage(outcome, player, computer);
-  updateEmoji(player, computer);
-  updateScore(playerScore, computerScore);
+    updateMessage(outcome, player, computer);
+    updateEmoji(player, computer);
+    updateScore(playerScore, computerScore);
+
+    if (playerScore === 5) endGame(true);
+    if (computerScore === 5) endGame(false);
+  }
+}
+
+function endGame(won) {
+  const endGameMessage = document.querySelector(".end-game");
+  const outcomeMessage = document.getElementById("game-outcome");
+  const playAgainButton = document.getElementById("play-again");
+
+  roundEnded = true;
+
+  if (won) {
+    outcomeMessage.textContent = "You won! Do you want to play again?";
+  } else {
+    outcomeMessage.textContent = "You lost. Do you want to play again?";
+  }
+
+  playAgainButton.addEventListener("click", resetGame);
+
+  endGameMessage.style.display = "block";
 }
 
 const buttons = document.querySelectorAll(".buttons button");
